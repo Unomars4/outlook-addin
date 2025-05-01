946807eb9e4c400cdd29ba380a26bee9cb9cea80
Une Nyide <une.nyide@gmail.com>
2025-05-01 12:49:20 +0200

    feat: init express server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,20 +1,39 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import { AppDataSource } from "./data-source";
+import { User } from "./entity/User";
+import * as express from "express";
+import * as dotenv from "dotenv";
+import { Request, Response } from "express";
+import { userRouter } from "./routes/user.routes";
+import "reflect-metadata";
+dotenv.config();
 
-AppDataSource.initialize().then(async () => {
+const app = express();
+app.use(express.json());
+app.use(errorHandler);
+const { PORT = 3000 } = process.env;
+app.use("/auth", userRouter);
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+app.get("*", (req: Request, res: Response) => {
+  res.status(505).json({ message: "Bad Request" });
+});
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
+AppDataSource.initialize()
+  .then(async () => {
+    console.log("Inserting a new user into the database...");
+    const user = new User();
+    user.firstName = "Timber";
+    user.lastName = "Saw";
+    user.email = "timber@gmail.com";
+    user.password = "dadaddadfd";
+    await AppDataSource.manager.save(user);
+    console.log("Saved a new user with id: " + user.id);
 
-    console.log("Here you can setup and run express / fastify / any other framework.")
+    console.log("Loading users from the database...");
+    const users = await AppDataSource.manager.find(User);
+    console.log("Loaded users: ", users);
 
-}).catch(error => console.log(error))
+    console.log(
+      "Here you can setup and run express / fastify / any other framework.",
+    );
+  })
+  .catch((error) => console.log(error));

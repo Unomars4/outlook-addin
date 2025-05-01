2755a5fa2c4fabaf9741d9a5c4a94ed2d105b558
Une Nyide <une.nyide@gmail.com>
2025-05-01 12:34:51 +0200

    feat: update user entity model

--- apps/server/src/entity/User.ts
+++ apps/server/src/entity/User.ts
@@ -1,18 +1,31 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
 
-@Entity()
+@Entity({ name: "users" })
 export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number
+  @Column()
+  firstName: string;
 
-    @Column()
-    firstName: string
+  @Column()
+  lastName: string;
 
-    @Column()
-    lastName: string
+  @Column({ nullable: false })
+  email: string;
 
-    @Column()
-    age: number
+  @Column({ nullable: false })
+  password: string;
 
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }

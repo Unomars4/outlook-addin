116f26676af23a981020e8dea7eb9895fdfa88f6
Une Nyide <une.nyide@gmail.com>
2025-05-01 15:08:04 +0200

    chore: update server dockerfile

---
+++ apps/server/src/migration/users.ts
@@ -0,0 +1,28 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class User1698321500514 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // this part you will add your self
+    await queryRunner.query(
+      ` 
+          --Table Definition
+          CREATE TABLE "users"  (
+            "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+            "firstName" character varying NOT NULL,
+            "lastName" character varying NOT NULL,
+            "email" character varying NOT NULL,
+            "password" character varying NOT NULL,
+            "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
+            "updatedAt" TIMESTAMP NOT NULL DEFAULT now(),
+            CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id")
+          )
+          `,
+    ),
+      undefined;
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // and this part
+    await queryRunner.query(`DROP TABLE "users"`, undefined);
+  }
+}
